77c6e3b23dd390f8de02ee3a75d65d8c74eac517
AdiHeiss <adrianfrancis.heiss@uzh.ch>
2022-10-18 14:47:55 +0200

    small changes to class diagram and very basic structure of Java project

--- Battleship/src/Main.java
+++ Battleship/src/Main.java
@@ -1,5 +1,22 @@
+import player.Computer;
+import player.User;
+
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        User user = new User();
+        Computer computer = new Computer();
+
+        Random rand = new Random();
+        boolean user_turn = rand.nextInt(2) == 0; // rand.nextInt(2) randomly returns 0 or 1
+        boolean game_over = false;
+        user.setFleet();
+        computer.setFleet();
+        while(!game_over){
+            if(user_turn){
+
+            }
+        }
     }
 }
---
+++ Battleship/src/block/BlockType.java
@@ -0,0 +1,4 @@
+package block;
+
+public enum BlockType {
+}
