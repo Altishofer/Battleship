c4b32d0e9be75ddc42f2302d5aac551b365cd0cc
AdiHeiss <116070324+AdiHeiss@users.noreply.github.com>
2022-11-04 12:19:21 +0100

    Update ShipFactory.java

--- Battleship/src/fleet/ShipFactory.java
+++ Battleship/src/fleet/ShipFactory.java
@@ -4,26 +4,13 @@ public class ShipFactory
 {
     public static Ship getShip(ShipType pType)
     {
-        if (pType == null){return null;}
-        if (pType.equals(ShipType.BATTLESHIP))
+        switch(pType) 
         {
-            return new Ship(ShipType.BATTLESHIP);
-        }
-        if (pType.equals(ShipType.CARRIER))
-        {
-            return new Ship(ShipType.CARRIER);
-        }
-        if (pType.equals(ShipType.PATROLBOAT))
-        {
-            return new Ship(ShipType.PATROLBOAT);
-        }
-        if (pType.equals(ShipType.SUBMARINE))
-        {
-            return new Ship(ShipType.SUBMARINE);
-        }
-        if (pType.equals(ShipType.NULL))
-        {
-            return SingletonNullShip.getInstance();
+            case BATTLESHIP: return new Ship(ShipType.BATTLESHIP);
+            case CARRIER:    return new Ship(ShipType.CARRIER);
+            case PATROLBOAT: return new Ship(ShipType.PATROLBOAT);
+            case SUBMARINE:  return new Ship(ShipType.SUBMARINE);
+            case NULL:       return SingletonNullShip.getInstance();
         }
         return null;
     }
